4f9fa396e2fc8cf1ea51b07231786de934b45da0
Ayush Dubey <dubeyaayush07@gmail.com>
2019-04-10 13:23:58 +0530

    Updates

---
+++ precache-manifest.9f7878782ce30906fa9e3cd2a6a19885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "305622eeafccc0bf7c37",
+    "url": "/entertainment-repository/static/js/runtime~main.8e7e1b6d.js"
+  },
+  {
+    "revision": "f1f186b0f9b6aac93497",
+    "url": "/entertainment-repository/static/js/main.a3ce683e.chunk.js"
+  },
+  {
+    "revision": "f572254fd1b6084c6466",
+    "url": "/entertainment-repository/static/js/2.1c479f53.chunk.js"
+  },
+  {
+    "revision": "f1f186b0f9b6aac93497",
+    "url": "/entertainment-repository/static/css/main.02bc2a8c.chunk.css"
+  },
+  {
+    "revision": "1f812e9d952be5739f7a816b3cc7a907",
+    "url": "/entertainment-repository/index.html"
+  }
+];
